da735ad0bd58c7ca2687536557f71acb840f2f07
manoharn725 <manoharnmanu725@gmail.com>
2025-01-08 11:08:11 +0530

    weather card

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import { useGetCurrentWeatherQuery } from "./store/api/currentWeatherApi";
 import SearchBar from "./components/SearchBar";
 // import WeatherForecastCrad from "./components/WeatherForecastCard";
+import { useUnixToLocalTimeContext } from "./components/context/UnixToLocalTime/useUnixToLocalTimeContext";
 import "./App.css";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState();
   const { data, isLoading, isError } = useGetCurrentWeatherQuery(searchTerm);
+  const {convertUnixToLocalTime, convertUnixToFormattedDate} = useUnixToLocalTimeContext();
 
-
-  // console.log(data);
   
+  console.log(data);
+  const convertToCelsius = (kelvin) => (kelvin - 273.15).toFixed(2);
   const handleSubmit = async (term) => {
     setSearchTerm(term);
   };
@@ -27,18 +29,53 @@ function App() {
             <span>🌤 {data?.main?.temp}°C</span>
           </div>
         </div>
-        <div className="settings">
-          <div className="dropdown">
-            <button className="dropdown-button">Theme</button>
-          </div>
-          <div className="dropdown">
-            <button className="dropdown-button">°F</button>
-          </div>
-        </div>
+        
       </header>
       <h1>Current Weather</h1>
-      <p>Temperature: {data?.main?.temp}°C</p>
-      <p>City: {data?.name}</p>
+      {/* weather-container */}
+      {/* {data.map((data) => ( */}
+      <div className="weather-container" key={data?.id}>
+        <div className="location">
+          {data?.name}, {data?.sys?.country}
+        </div>
+        <img
+          className="icon"
+          src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`}
+          alt={data?.weather[0]?.description}
+        />
+        <div className="temperature">
+          {convertToCelsius(data?.main?.temp)}&#8451;
+        </div>
+        <div className="description">{data?.weather[0]?.description}</div>
+
+        <div className="details">
+          <div className="detail">Humidity: {data?.main?.humidity}</div>
+          <div className="detail">Pressure: {data?.main?.pressure} hPa</div>
+          <div className="detail">Wind Speed: {data?.wind?.speed} m/s</div>
+          <div className="detail">Visibility: {data?.visibility / 1000} km</div>
+        </div>
+
+        <div className="sun-info">
+          <div>
+            <img
+              src="https://img.icons8.com/emoji/48/000000/sunrise-emoji.png"
+              alt="Sunrise"
+            />{" "}
+            {convertUnixToLocalTime(data?.sys?.sunrise, data?.timezone)}
+          </div>
+          <div>
+            <img
+              src="https://img.icons8.com/emoji/48/000000/sunset-emoji.png"
+              alt="Sunset"
+            />{" "}
+            {convertUnixToLocalTime(data?.sys?.sunset,data?.timezone)}
+          </div>
+        </div>
+
+        <footer>Last updated: {convertUnixToFormattedDate(data?.dt)}</footer>
+      </div>
+      {/* ))} */}
+
       {/* <WeatherForecastCrad /> */}
     </>
   );
