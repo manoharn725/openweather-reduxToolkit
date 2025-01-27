593fddab14eb61396fa29de1bfc2e4835197b77d
manoharn725 <manoharnmanu725@gmail.com>
2025-01-27 10:56:13 +0530

    unix to local context correction

--- src/context/UnixToLocalTime/UnixToLocalTimeContext.jsx
+++ src/context/UnixToLocalTime/UnixToLocalTimeContext.jsx
@@ -3,23 +3,34 @@ import { createContext } from "react";
 export const UnixToLocalTimeContext = createContext();
 
 export const UnixToLocalTimeContextProvider = ({ children }) => {
-    const convertUnixToLocalTime = (unixTimestamp, timezone=19800) => {
-      const date = new Date((unixTimestamp + timezone) * 1000);
-      // console.log(date.toLocaleString)
-      return date.toLocaleString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      });
-    };
-    const convertUnixToFormattedDate = (unixTimestamp) => {
-        const date = new Date(unixTimestamp * 1000);
-        // console.log(date.toUTCString)
-        return date.toUTCString().split(' ').splice(0,4).join(' ')
-    }
-    return (
-      <UnixToLocalTimeContext.Provider value={{convertUnixToLocalTime, convertUnixToFormattedDate}}>
-        {children}
-      </UnixToLocalTimeContext.Provider>
-    );
+  const convertUnixToLocalTime = (unixTimestamp = 1737854757) => {
+    const date = new Date(unixTimestamp * 1000);
+    return date.toLocaleString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: "Asia/Kolkata",
+    });
   };
+  const convertUnixToFormattedDate = (unixTimestamp) => {
+    const date = new Date(unixTimestamp * 1000);
+    return date.toUTCString().split(" ").splice(0, 4).join(" ");
+  };
+
+  const convertTo12HoursFormate = (time24) => {
+    const [hours, minutes] = time24.split(":").map(Number);
+    const period = hours >= 12 ? "PM" : "AM";
+    const hours12 = hours % 12 || 12; // Convert 0 to 12 for midnight
+    const time12 = `${hours12}:${minutes
+      .toString()
+      .padStart(2, "0")} ${period}`;
+      return time12;
+  };
+  return (
+    <UnixToLocalTimeContext.Provider
+      value={{ convertUnixToLocalTime, convertUnixToFormattedDate, convertTo12HoursFormate }}
+    >
+      {children}
+    </UnixToLocalTimeContext.Provider>
+  );
+};
