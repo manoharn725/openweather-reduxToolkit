4c848031543a73411aa16b17b824cc6d5bb7ea98
manoharn725 <manoharnmanu725@gmail.com>
2025-01-31 05:12:09 +0530

    destructuring

--- src/context/ConvertToCelsius/ConvertToCelsiusContext.jsx
+++ src/context/ConvertToCelsius/ConvertToCelsiusContext.jsx
@@ -5,7 +5,7 @@ export const ConvertToCelsiusContext = createContext();
 export const ConvertToCelsiusProvider = ({ children }) => {
   const convertToCelsius = (kelvin) => (kelvin - 273.15).toFixed(2);
   return (
-    <ConvertToCelsiusContext.Provider value={convertToCelsius}>
+    <ConvertToCelsiusContext.Provider value={{ convertToCelsius }}>
       {children}
     </ConvertToCelsiusContext.Provider>
   );
